4b243dedca80e7b1aaaa1c7732dcda60e7bc05d3
Kolinsky <miroslav.kolinsky@siemens.com>
2019-08-16 11:19:20 +0000

    mod for esp32, now compiles, TODO gpio

--- ESP32_diversity.ino
+++ ESP32_diversity.ino
@@ -17,8 +17,7 @@
 //
 // ====================================================================
 
-
-#include "user_interface.h"
+//#include "user_interface.h"
 
 //N is the number of video lines that are averaged over to decide which video source has the highest quality
 #define N 500
@@ -27,22 +26,21 @@
 //#define SERIAL_DEBUG
 
 //Pin connected to the LM1881 sync output for video signal "A"
-#define PIN_SyncA D1  //GPIO5=D1
+#define PIN_SyncA 12 //GPIO5=D1
 
 //Pin connected to the LM1881 sync output for video signal "B"
-#define PIN_SyncB D2  //GPIO4=D2
+#define PIN_SyncB 13
 
 //Pin connected to the LM1881 sync output for video signal "C"
-#define PIN_SyncC D6
+#define PIN_SyncC 14
 
 //Pin connected to the MAX4545 (switch) pin that selects the desired source
-#define PIN_selectA D3    //GPIO0=D3
-#define PIN_selectB D7    //GPIO13=D7
-#define PIN_selectC D5    //GPIO14=D5
+#define PIN_selectA 16
+#define PIN_selectB 17
+#define PIN_selectC 18
 
-#define PIN_ESP_LED D0    //GPIO16=D0
-#define PIN_ESP_LED2 D4    //GPIO2=D4
-
+#define PIN_ESP_LED 19
+#define PIN_ESP_LED2 20
 
 // keyword "Volatile" means that this variable may be changed by a routine called by some interrupt.
 volatile unsigned long lastA; // Time of the last sync signal
@@ -58,7 +56,7 @@ unsigned long now;
 
 #define t0 63
 //const long t0 = 63;
-const long unsigned mus = t0 * N; //Period to update the selected source, in micro sec.
+const long unsigned mus = t0 * N;     //Period to update the selected source, in micro sec.
 const long unsigned mis = mus / 1000; //same as above, in milli sec.
 
 #define sourceA 0
@@ -69,90 +67,116 @@ int selected;
 unsigned long lastMillis;
 
 //-----------------------------------------------------------------
-class ExpAverage {
-  private:
-    volatile float x;
-    const float a, b;
-  public:
-    ExpAverage(int n): a(1.0f / n), b(1.0f - a) { //exponential average over the ~n previous values
-      x = 0.0f;
-    }
-    inline float getAverage() {
-      return x;
-    }
-    void addValue(unsigned long v) {
-      x = a * v + b * x;
-    }
+class ExpAverage
+{
+private:
+  volatile float x;
+  const float a, b;
+
+public:
+  ExpAverage(int n) : a(1.0f / n), b(1.0f - a)
+  { //exponential average over the ~n previous values
+    x = 0.0f;
+  }
+  inline float getAverage()
+  {
+    return x;
+  }
+  void addValue(unsigned long v)
+  {
+    x = a * v + b * x;
+  }
 };
 //-----------------------------------------------------------------
-ExpAverage A(N), B(N), C(N);//Average video line duration for the different sources
+ExpAverage A(N), B(N), C(N); //Average video line duration for the different sources
 #ifdef SERIAL_DEBUG
-ExpAverage AA(N), BB(N), CC(N);//Average video line duration for the different sources
+ExpAverage AA(N), BB(N), CC(N); //Average video line duration for the different sources
 #endif
 //-----------------------------------------------------------------
 const int CLEAR = (1 << PIN_selectA) + (1 << PIN_selectB) + (1 << PIN_selectC);
-int Bin[3] = {1 << PIN_selectA , 1 << PIN_selectB, 1 << PIN_selectC};
-inline void SwitchTo(int s) {
-  if (selected != s) {
+int Bin[3] = {1 << PIN_selectA, 1 << PIN_selectB, 1 << PIN_selectC};
+inline void SwitchTo(int s)
+{
+  if (selected != s)
+  {
     selected = s;
-    GPOC = CLEAR;//Clear PIN_selectA, PIN_selectB and PIN_selectC simultaneously
-    GPOS = Bin[s];
+    //GPOC = CLEAR; //Clear PIN_selectA, PIN_selectB and PIN_selectC simultaneously
+    //GPOS = Bin[s];
   }
 }
 //-----------------------------------------------------------------
-class PeriodicTask {
-  private:
-    os_timer_t myTimer;
-    int t;
-  public:
-    //-----------------------------
-    static void DoTask(void *pArg) {//static needed here, because below we want to pass a pointer to this member function
-      const unsigned long  now = micros();
-      if (now - lastA > mus) {
-        //if no pulse was received since last update, put the value 'mus' into the average
-        //... so, if no pulse is received at all (disconnected cable, etc.à, the average line duration will be 'mus=N*64' (very large compared to 64) so this
-        // source will not be selected.
-        A.addValue(mus); lastA = now;
+
+class PeriodicTask
+{
+private:
+  ETSTimer myTimer;
+  int t;
+
+public:
+  //-----------------------------
+  static void DoTask(void *pArg)
+  { //static needed here, because below we want to pass a pointer to this member function
+    const unsigned long now = micros();
+    if (now - lastA > mus)
+    {
+      //if no pulse was received since last update, put the value 'mus' into the average
+      //... so, if no pulse is received at all (disconnected cable, etc.à, the average line duration will be 'mus=N*64' (very large compared to 64) so this
+      // source will not be selected.
+      A.addValue(mus);
+      lastA = now;
+    }
+    if (now - lastB > mus)
+      B.addValue(mus), lastB = now;
+    if (now - lastC > mus)
+      C.addValue(mus), lastC = now;
+
+    //Get the average deviation (from ~63 micro seconds) of the line durations
+    const float avA = A.getAverage();
+    const float avB = B.getAverage();
+    const float avC = C.getAverage();
+
+    if (avB < avA)
+    {
+      //The best is B or C
+      if (avC < avB)
+      {
+        SwitchTo(sourceC);
+        //Serial.println(avA); Serial.println(avB); Serial.println(avC);
       }
-      if (now - lastB > mus) B.addValue(mus), lastB = now;
-      if (now - lastC > mus) C.addValue(mus), lastC = now;
-
-      //Get the average deviation (from ~63 micro seconds) of the line durations
-      const float avA = A.getAverage();
-      const float avB = B.getAverage();
-      const float avC = C.getAverage();
-
-      if (avB < avA) {
-        //The best is B or C
-        if (avC < avB)
-        {
-          SwitchTo(sourceC);
-          //Serial.println(avA); Serial.println(avB); Serial.println(avC);
-        }
-        else {
-          SwitchTo(sourceB);
-        }
-
-      } else {//The best is A or C
-        if (avA < avC)
-          SwitchTo(sourceA); else {
-            SwitchTo(sourceC);
-            //Serial.println(avA); Serial.println(avB); Serial.println(avC);
-          }
+      else
+      {
+        SwitchTo(sourceB);
       }
     }
-    //-----------------------------
-    PeriodicTask () {
-      os_timer_setfn(&myTimer, DoTask, NULL);
+    else
+    { //The best is A or C
+      if (avA < avC)
+        SwitchTo(sourceA);
+      else
+      {
+        SwitchTo(sourceC);
+        //Serial.println(avA); Serial.println(avB); Serial.println(avC);
+      }
     }
-    void Start(int t, bool repeat = true) {
-      os_timer_arm(&myTimer, t, repeat);
-    }
+  }
+
+  //-----------------------------
+  PeriodicTask()
+  {
+
+    ets_timer_setfn(&myTimer, DoTask, NULL);
+  }
+  void Start(int t, bool repeat = true)
+  {
+    ets_timer_arm(&myTimer, t, repeat);
+  }
 };
 PeriodicTask T;
+
 //--------Count---------------------------------------------------------
-void CountA() {
-  const unsigned long  now = micros();
+void CountA()
+{
+  const unsigned long now = micros();
   const long period = now - lastA;
   lastA = now;
   A.addValue(abs(period - t0));
@@ -160,7 +184,9 @@ void CountA() {
   AA.addValue(period);
 #endif
 }
-void CountB() {
+
+void CountB()
+{
   const unsigned long now = micros();
   const long period = now - lastB;
   lastB = now;
@@ -169,7 +195,9 @@ void CountB() {
   BB.addValue(period);
 #endif
 }
-void CountC() {
+
+void CountC()
+{
   const unsigned long now = micros();
   const long period = now - lastC;
   lastC = now;
@@ -180,7 +208,8 @@ void CountC() {
 }
 //-----------------------------------------------------------------
 
-void setup() {
+void setup()
+{
   //ESP.wdtDisable(); // To avoid some resets due to the software Watch Dog Timer
 
   //WiFi.mode(WIFI_OFF); WiFi.forceSleepBegin();
@@ -203,10 +232,14 @@ void setup() {
 
   selected = sourceB;
   //Some blibking to start
-  for (int i = 0; i < 3; i++) {
-    SwitchTo(sourceA); delay(250);
-    SwitchTo(sourceB); delay(250);
-    SwitchTo(sourceC); delay(250);
+  for (int i = 0; i < 3; i++)
+  {
+    SwitchTo(sourceA);
+    delay(250);
+    SwitchTo(sourceB);
+    delay(250);
+    SwitchTo(sourceC);
+    delay(250);
   }
   //Declare 3 interrupts, to measure the duration between horizontal sync. pulses
   //on the 3 sources
@@ -218,39 +251,52 @@ void setup() {
 }
 //-----------------------------------------------------------------
 int z = 1;
-void loop() {
-  if (millis() > lastMillis + 1000) {
-    ESP.wdtFeed();
+void loop()
+{
+  if (millis() > lastMillis + 1000)
+  {
+//    ESP.wdtFeed();
+    //esp.wdtFeed();
     // Some blinking of the bluit-in led
-    if (z == 1) digitalWrite(PIN_ESP_LED, HIGH); else digitalWrite(PIN_ESP_LED, LOW); z = -z;
+    if (z == 1)
+      digitalWrite(PIN_ESP_LED, HIGH);
+    else
+      digitalWrite(PIN_ESP_LED, LOW);
+    z = -z;
     lastMillis = millis();
 #ifdef SERIAL_DEBUG
     //Prints some debug information to the serial output, every 1000 milli sec.
-    Serial.print("Time: "); Serial.println(millis());
-    if (selected == sourceA)     Serial.print("=>");
+    Serial.print("Time: ");
+    Serial.println(millis());
+    if (selected == sourceA)
+      Serial.print("=>");
     Serial.print("A:");
     const float avA = A.getAverage();
     Serial.print(avA);
     const float avAA = AA.getAverage();
-    Serial.print(" - "); Serial.println(avAA);
+    Serial.print(" - ");
+    Serial.println(avAA);
 
-    if (selected == sourceB)     Serial.print("=>");
+    if (selected == sourceB)
+      Serial.print("=>");
     Serial.print("B:");
     const float avB = B.getAverage();
     Serial.print(avB);
     const float avBB = BB.getAverage();
-    Serial.print(" - "); Serial.println(avBB);
+    Serial.print(" - ");
+    Serial.println(avBB);
 
-    if (selected == sourceC)     Serial.print("=>");
+    if (selected == sourceC)
+      Serial.print("=>");
     Serial.print("C:");
     const float avC = C.getAverage();
     Serial.print(avC);
     const float avCC = CC.getAverage();
-    Serial.print(" - "); Serial.println(avCC);
-
+    Serial.print(" - ");
+    Serial.println(avCC);
 
     Serial.println("");
 #endif
   }
-  yield();
+  yield(); //call background stuff
 }
